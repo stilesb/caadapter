60eb66533099ea937c98fa9f5cf52851975502e7
Brandon Stiles <stilesbr1@gmail.com>
2017-07-25 10:11:37 -0400

    devops(docker): Updated compose.sh commands

--- deployment/bin/compose.sh
+++ deployment/bin/compose.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
 
-docker-compose build
-docker-compose up
+set -e
+set -x
+
+export PROJECT_DIR=$(PWD)
+export COMPOSE_FILE=$PROJECT_DIR/deployment/docker-compose.yml
+export COMPOSE_PROJECT_NAME=caadapter
+
+docker_build() {
+    docker-compose -f $COMPOSE_FILE build
+}
+
+docker_up() {
+    docker-compose -f $COMPOSE_FILE up -d
+}
+
+docker_rm() {
+    docker-compose -f $COMPOSE_FILE stop
+    docker-compose -f $COMPOSE_FILE rm
+}
+
+case "$1" in
+    "build")
+	docker_build
+	;;
+    "up")
+	docker_up
+	;;
+    "bounce")
+	docker_build
+	docker_up
+	;;
+    "rm")
+	docker_rm
+	;;
+    *)
+	echo "Usage: ./deployment/bin/compose.sh [build | up | rm]"
+	;; 
+esac
